c239cec470fb97d0302987436bcf049c63810224
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-05 20:49:02 +0100

    commit return statement

---
+++ Day-10/days_month.py
@@ -0,0 +1,28 @@
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True #Input is a leap year
+      else:
+        return False #Input is not a leap year
+    else:
+      return True #Input is a leap year
+  else:
+    return False #Input is not a leap year
+
+def days_in_month(yr,mnt):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap(yr) == True:
+      month_days[1] = 29
+      return month_days[(mnt - 1)]
+  elif is_leap(yr) == False:
+      return month_days[(mnt - 1)]
+  
+      
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
---
+++ Day-10/function_ouput.py
@@ -0,0 +1,10 @@
+from turtle import title
+
+
+name = input("Enter your first name: ")
+scond = input("Enter your last name: ")
+
+def format_name(l_name):
+ return title(l_name)
+
+format_name(l_name=scond)
