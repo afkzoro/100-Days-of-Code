73e0845fa8ff5bef110bd842e7b7fa23e47802c8
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-17 14:04:10 +0100

    future fix

--- Day-14/higher_lower.py
+++ Day-14/higher_lower.py
@@ -1,25 +1,30 @@
 #Using a Dictionary of values
-from logging.config import valid_ident
 import random
 from game_data import data
 from art import vs
+from art import logo
+import os
 
-def searchA(list):
+def higher_lower(list):
     flag = True 
-    first_search = random.sample(list, 1)
+    first_search = random.choice(list)
     score = 0
+    print(logo)
     
     while flag != False:
-        print(f"Compare A: {first_search[0]['name']}, a {first_search[0]['description']}, from {first_search[0]['country']}")
-        first_value = first_search[0]['follower_count']
+        print(f"Compare A: {first_search['name']}, a {first_search['description']}, from {first_search['country']}")
+        first_value = first_search['follower_count']
+        
+        second_search = random.choice(list)
+        if second_search == first_search:
+            second_search = random.choice(list)
         
         print(vs)
         
-        second_search = random.sample(list, 1)
-        print(f"Against A: {second_search[0]['name']}, a {second_search[0]['description']}, from {second_search[0]['country']}")
-        second_val = second_search[0]['follower_count']
+        print(f"Against B: {second_search['name']}, a {second_search['description']}, from {second_search['country']}")
+        second_val = second_search['follower_count']
         
-        decide = input("Who has more followers? Type A or B: ").upper()
+        decide = input("Who has more followers? Type 'A' or 'B': ").upper()
         
         def compare(val1, val2):
             if val1 > val2:
@@ -31,20 +36,28 @@ def searchA(list):
             if compare(val1=first_value, val2=second_val) == True:
                 first_search = first_search
                 score += 1
+                #os.system('cls')
+            elif compare(val1=first_value, val2=second_val) == 0:
+                first_search = first_search
+                score += 1
+                 
             elif compare(val1=first_value, val2=second_val) == False:
-                print("You lose")
+                print(f"Sorry you lose. Final score is {score}")
                 flag = False
+                #os.system('cls')
                 break
         
         elif decide == "B":
             if compare(val1=first_value, val2=second_val) == False:
                 first_search = second_search
                 score += 1
+                #os.system('cls')
             elif compare(val1=first_value, val2=second_val) == True:
-                print("You lose")
+                os.system('cls')
+                print(f"Sorry you lose. Fianl score is {score}")
                 flag = False
                 break
                 
             
 
-searchA(data)
+higher_lower(data)
--- test.py
+++
@@ -1,6 +0,0 @@
-import random
-
-fic = [2, 3, 5, 6, 8, 9]
-pick = random.sample(fic, 1)
-
-print(fic)
