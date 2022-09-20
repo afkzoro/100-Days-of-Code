d5621ff80c0df8a115574690beac66fe697f82fe
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-20 21:03:31 +0100

    commit

--- Day-16/149 oop-coffee-machine-start/oop-coffee-machine-start/main.py
+++ Day-16/149 oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -3,16 +3,25 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 flag = True
+    
+maker = CoffeeMaker()
+money = MoneyMachine()
+menu = Menu()
+#menuCall = MenuItem()
 
 while flag != False:
-    user = input(f"What would you like (espresso/latte/cappuccino): ").lower()
-    maker = CoffeeMaker()
-    rep = MoneyMachine()
+    user = input("What would you like (espresso/latte/cappuccino): ").lower()
     
     if user == "report":
         maker.report()
-        rep.report()
-        
-    if maker.is_resource_sufficient(user) == True:
+        money.report()
+
+    elif user == "off":
+        flag = False
     
-            
+    else:
+        choice = menu.find_drink(user)
+        if maker.is_resource_sufficient(choice) == True:
+            pay = money.process_coins()
+            if money.make_payment(choice.cost) == True:
+                
