0d94b6c3d816ca6371e0cc53204c12f26ea353d5
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-20 11:38:08 +0100

    commit

--- Day-16/149 oop-coffee-machine-start/oop-coffee-machine-start/main.py
+++ Day-16/149 oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -2,3 +2,15 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+flag = True
+
+while flag != False:
+    user = input(f"What would you like (espresso/latte/cappuccino): ").lower()
+    
+    if user == "report":
+        maker = CoffeeMaker()
+        maker.report()
+        rep = MoneyMachine()
+        rep.report()
+    
+            
