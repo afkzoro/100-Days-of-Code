d2723274caa2336c2959aaac6b1a5a8c03bb9818
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-07 19:52:44 +0100

    commit

--- Day-15/brackets.py
+++ Day-15/brackets.py
@@ -7,9 +7,9 @@ def is_paired(myStr: str):
             record.append(i)
         elif i in close_brackets:
             pos_index = close_brackets.index(i)
-            if ((len(record) > 0) and (open_brackets[pos_index] == record[len(record)-1])):
+            if len(record) > 0 and open_brackets[pos_index] == record[-1]:
                 record.pop()
             return False
-    if len(record) == 0:
+    if not record:
         return True
     return False
