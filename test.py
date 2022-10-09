e56246f775289e65cc0e7adb613124534901dad6
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-09 11:36:20 +0100

    commit

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-if 12 == 48/3 or 12 is 12:
-    print("Holberton")
-else:
-    print("School")
+def square(user):
+    if user > 0 and user <= 64:
+        return (f"The number of grains on square {user} is {user}")
+    else:
+        raise ValueError("Square must be between 1 and 64")
+
+def total():
+    sum = 0
+    for i in range(1, 65):
+        sum += i
+    return sum
