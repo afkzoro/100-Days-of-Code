88ad554b2ceb63fd1e91ece12fbc9f98ff4e5eeb
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-09 12:30:11 +0100

    commit

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-def pangram(sentence):
-    return not set('abcdefghijklmnopqrstuvwxyz') - set(sentence.lower())
+def steps(number):
+    count = 0
+    div = number
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    if number > 0:
+        while div != 1:
+            if div % 2 == 0:
+                div = div / 2
+                count += 1
+            div = 3 * div + 1
+            count += 1
+    return count
