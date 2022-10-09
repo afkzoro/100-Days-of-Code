c94250c53c340086570521ec99c174069ba3a096
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-09 11:59:09 +0100

    commit

--- test.py
+++ test.py
@@ -1,23 +1,6 @@
-def square(user):
-    """_summary_
- 
-    Args:
-        user (_type_): _description_
- 
-    Raises:
-        ValueError: _description_
- 
-    Returns:
-        _type_: _description_
-    """
-    if user <= 0 or user > 64:
-        raise ValueError("square must be between 1 and 64")
-    return 2**(user - 1)
-    
-def total():
-    """_summary_
- 
-    Returns:
-        _type_: _description_
-    """
-    return 2**64 -1
+def is_isogram(word):
+    result = word
+    if "-" in word:
+        result = word.replace("-", " ")
+    result = "".join(result.split())
+    return len(result) == len(set(result.lower()))
