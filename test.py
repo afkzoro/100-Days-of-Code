12dc96508acdd17d973822913f8f1302040096a8
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-09 12:02:42 +0100

    commit

--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-def is_isogram(word):
-    result = word
-    if "-" in word:
-        result = word.replace("-", " ")
-    result = "".join(result.split())
-    return len(result) == len(set(result.lower()))
+def pangram(sentence):
+    return not set('abcdefghijklmnopqrstuvwxyz') - set(sentence.lower())
