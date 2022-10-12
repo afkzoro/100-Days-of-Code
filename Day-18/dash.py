db5628a153c5b544b053b37fe426e052bab17649
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-12 09:35:25 +0100

    commit

---
+++ Day-18/dash.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+my_turtle  = Turtle()
+
+for i in range(10): 
+    my_turtle.forward(10)
+    my_turtle.penup()
+    my_turtle.forward(10)
+    my_turtle.pendown()
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
---
+++ Day-18/square.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+my_turtle  = Turtle()
+
+for i in range(4):   
+    my_turtle.forward(100)
+    my_turtle.rt(90)
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
